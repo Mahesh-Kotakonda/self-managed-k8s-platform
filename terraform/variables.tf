286e1f82284671ce6f057304951df947e7dc039a
Mahesh Kotakonda <mahesh04815@gmail.com>
2026-01-11 10:53:37 +0530

    Update variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,39 @@
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for all nodes"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "worker_count" {
+  description = "Number of Kubernetes worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "key_name" {
+  description = "Name of the existing AWS EC2 key pair"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for Ubuntu (20.04 or 22.04 recommended)"
+  type        = string
+}
