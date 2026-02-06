3716fe5c85f1b309df81f6bea281899cd17b9e90
Mahesh Kotakonda <mahesh04815@gmail.com>
2026-02-06 12:45:47 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -193,6 +193,14 @@ resource "aws_security_group" "nodes" {
   name   = "k8s-nodes-sg"
   vpc_id = aws_vpc.k8s.id
 
+  # Allow NLB → Pod traffic (IP target mode)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"] # VPC CIDR
+  }
+
   ingress {
     from_port   = 80
     to_port     = 80
@@ -289,6 +297,7 @@ resource "aws_instance" "workers" {
     "kubernetes.io/cluster/self-managed-k8s" = "owned"
   }
 }
+
 
 
 
